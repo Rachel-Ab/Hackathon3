4a4fbe9c1429d7c4ff97fffe6fbfa066049fea8d
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-04-26 09:48:45 +0200

    Update settings.py

--- quizbot/settings.py
+++ quizbot/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5zobuggloom=ia8v$pgo!hk$nlfr05if65#eqzko403*ns*1^o'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
